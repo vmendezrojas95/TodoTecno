7de26a618c830f0de717c4ec190d57c71ecd0a86
vmendezrojas95 <vmendezrojas@frba.utn.edu.ar>
2023-11-08 12:09:29 -0300

    Big update

--- TodoTecno/settings.py
+++ TodoTecno/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'categoria',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -116,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' # CHECK THIS
+STATIC_ROOT = BASE_DIR  /'static'
+STATICFILES_DIRS = [
+     'TodoTecno/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ categoria/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    categoria_nombre = models.CharField(max_length=50, unique=True)
+    descripcion = models.CharField(max_length=200, blank=True, null=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    categoria_imagen = models.ImageField(upload_to='fotos/categoria', blank=True, null=True)
+    
+    def __str__(self):
+        return self.categoria_nombre
