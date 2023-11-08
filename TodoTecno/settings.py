d9115fa7b1bb928ef53d080d76f89bfe96508dbb
vmendezrojas95 <vmendezrojas@frba.utn.edu.ar>
2023-11-08 17:13:16 -0300

    Parche 1.2

--- TodoTecno/settings.py
+++ TodoTecno/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'categoria',
+    'cuentas',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'TodoTecno.wsgi.application'
+
+AUTH_USER_MODEL = 'cuentas.Account'
 
 
 # Database
--- categoria/models.py
+++ categoria/models.py
@@ -7,5 +7,10 @@ class Categoria(models.Model):
     slug = models.SlugField(max_length=50, unique=True)
     categoria_imagen = models.ImageField(upload_to='fotos/categoria', blank=True, null=True)
     
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['categoria_nombre']
+
     def __str__(self):
         return self.categoria_nombre
